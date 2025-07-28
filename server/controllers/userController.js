9a79810f99af874dd734051be94b7e1257e846cf
Ojas Goyal <ojasgoyal05@gmail.com>
2025-07-28 13:10:01 +0530

    update password and edit verify

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,6 +1,7 @@
 import User from "../models/User.js";
 import cloudinary from "../config/cloudinary.js";
 import streamifier from "streamifier";
+import bcrypt from "bcryptjs"
 
 export const getUserProfile = async (req ,res) => {
     try {
@@ -17,7 +18,7 @@ export const getUserProfile = async (req ,res) => {
 
 export const updateUserProfile = async (req,res)=>{
     try {
-        const {username} = req.params
+        const {userId} = req.user
         const allowedFields = ["name", "bio"];
 
         const updates = Object.keys(req.body);
@@ -27,7 +28,7 @@ export const updateUserProfile = async (req,res)=>{
             return res.status(400).json({ error: "Invalid fields in update" });
         }
 
-        const user = await User.findOne({ username });
+        const user = await User.findById(userId);
         if (!user) return res.status(404).json({ error: "User not found" });
 
         const update = {};
@@ -39,30 +40,30 @@ export const updateUserProfile = async (req,res)=>{
             if (user.avatar?.public_id){
                 await cloudinary.uploader.destroy(user.avatar.public_id);
             }
+            
+            const streamUpload = () => new Promise((resolve, reject) => {
+                const stream = cloudinary.uploader.upload_stream(
+                    {
+                        folder: "avatars",
+                        transformation: [{ width: 500, height: 500, crop: "limit" }],
+                    },
+                    (error,result) => {
+                        if(result ) resolve (result);
+                        else reject(error); 
+                    }
+                )
+                streamifier.createReadStream(req.file.buffer).pipe(stream);
+            })
+
+            const result = await streamUpload();
+            update.avatar = {
+                url: result.secure_url,
+                public_id: result.public_id,
+            };
         }
-        
-        const streamUpload = () => new Promise((resolve, reject) => {
-            const stream = cloudinary.uploader.upload_stream(
-                {
-                    folder: "avatars",
-                    transformation: [{ width: 500, height: 500, crop: "limit" }],
-                },
-                (error,result) => {
-                    if(result ) resolve (result);
-                    else reject(error); 
-                }
-            )
-            streamifier.createReadStream(req.file.buffer).pipe(stream);
-        })
-
-        const result = await streamUpload();
-        update.avatar = {
-            url: result.secure_url,
-            public_id: result.public_id,
-        };
 
         
-        const updatedUser = await User.findOneAndUpdate({username},update,{
+        const updatedUser = await User.findByIdAndUpdate(userId,update,{
             new:true,
             runValidators: true,
 
@@ -73,5 +74,34 @@ export const updateUserProfile = async (req,res)=>{
 
     } catch (error) {
         res.status(500).json({ error: "Server error" });
+    }
+}
+
+export const updatePassword = async (req,res) => {
+    try {
+        const { userId } = req.user
+        const { currPassword , newPassword } = req.body
+
+        if(!currPassword || !newPassword){
+            return res.status(400).json({ error: "Both current and new passwords are required" });
+        }
+        if(currPassword === newPassword){
+            return res.status(400).json({ error: "Both current and new passwords are same" });
+        }
+
+        const user = await User.findById(userId);
+        if (!user) return res.status(404).json({ error: "User not found" });
+
+        const isMatch = await bcrypt.compare(currPassword, user.password);
+        if (!isMatch) return res.status(401).json({ error: "Current password is incorrect" });
+
+        const hashedPassword = await bcrypt.hash(newPassword, 10);
+        user.password = hashedPassword;
+        await user.save();
+
+        res.json({ message: "Password updated successfully" });
+
+    } catch (error) {
+        res.status(500).json({ error: "Server error: Can't Update Password" });
     }
 }
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,12 +1,13 @@
 import express from "express"
-import { getUserProfile , updateUserProfile } from "../controllers/userController.js"
+import { getUserProfile , updateUserProfile ,updatePassword} from "../controllers/userController.js"
 import { verifyToken } from "../middlewares/authMiddleware.js"
 import { authorizeSelf } from "../middlewares/authorizeSelf.js"
 import upload from "../middlewares/multer.js"
 
 const router = express.Router();
 
+router.put("/updatepassword", verifyToken, updatePassword);
+router.put("/edit", verifyToken, upload.single("avatar"), updateUserProfile);
 router.get("/:username", getUserProfile);
-router.put("/:username", verifyToken,upload.single("avatar"), authorizeSelf, updateUserProfile);
 
 export default router;
