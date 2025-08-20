7461c2d8aed4c70fa48f8b74763b1ee528999968
Ojas Goyal <ojasgoyal05@gmail.com>
2025-08-20 16:53:32 +0530

    CORS

--- server/index.js
+++ server/index.js
@@ -22,7 +22,12 @@ const PORT = process.env.PORT || 5000
 const MONGO_URI = process.env.MONGO_URI
 
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true, 
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
 
