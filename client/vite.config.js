28aa425117844950d448f0ffa48978a19238d696
Ojas Goyal <ojasgoyal05@gmail.com>
2025-11-25 21:22:13 +0530

    PWA Setup

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,12 +1,38 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "MovieGeek",
+        short_name: "MovieGeek",
+        start_url: "/",
+        display: "standalone",
+        background_color: "#ffffff",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "/icons/icon-192.png",
+            sizes: "192x192",
+            type: "image/png"
+          },
+          {
+            src: "/icons/icon-512.png",
+            sizes: "512x512",
+            type: "image/png"
+          }
+        ]
+      }
+    })
+  ],
   server: {
     port: 5173,
     strictPort: true
   },
-  base: '/' // Ensure correct base path for production builds
+  base: '/'
 })
